5c70a51dcd1c9f1b48c79ba641589725748b7ecf
yianxd <jean4520rodriguez@gmail.com>
2023-10-12 17:19:26 -0500

    clases...fallido

--- proyecto/proyecto/views.py
+++ proyecto/proyecto/views.py
@@ -1,5 +1,6 @@
 from django.http import request,HttpResponse
 from django.template import Template,Context
+import datetime
 
 def primero(request):
     doc_e=open("C:/Users/Aprendiz/Pictures/prueba/proyecto/proyecto/pantillas/primera.html")
@@ -13,11 +14,26 @@ def primero(request):
 
 def segundo(request):
 
+    class Persona:
+        def __init__(self,nombre,documento):
+            self.nombre=nombre
+            self.documento=documento
+
+        def getNombre(self):
+            return self.nombre
+
+
+
     nombre='juan'
+    obj1=Persona(nombre,12334)
+
+    tiempo=datetime.datetime.now()
     doc_e=open("C:/Users/Aprendiz/Pictures/prueba/proyecto/proyecto/pantillas/segundo.html")
     #doc_e=open("./proyecto/pantillas/primera.html")
     plt=Template(doc_e.read())
     doc_e.close()
-    ctx=Context({'nombre':nombre})
+    ctx=Context({   'nombre':nombre,
+                    'tiempo':tiempo,
+                    'clase':obj1})
     doc=plt.render(ctx)
     return HttpResponse(doc)
